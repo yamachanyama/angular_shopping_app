f231e935b399f4499b9756489cec30a959822d6b
shoheyheyhey <shohey.asada0525@gmail.com>
2017-09-14 00:10:13 +0900

    緑川：退会完了 新規作成

--- src/app/user/unsubscribefinish.component.ts
+++ src/app/user/unsubscribefinish.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-unsubscribefinish',
-  templateUrl: './unsubscribefinish.component.html'
+  templateUrl: './unsubscribefinish.component.html',
+  styleUrls: ['./unsubscribefinish.component.css']
 })
 
 export class UnsubscribeFinishComponent {
