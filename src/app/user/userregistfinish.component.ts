34f73be35d4f073a555f46297611a651fd90f67d
shoheyheyhey <shohey.asada0525@gmail.com>
2017-12-03 23:41:20 +0900

    緑川：レイアウト修正

--- src/app/user/userregistfinish.component.ts
+++ src/app/user/userregistfinish.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-userregistfinish',
-  templateUrl: './userregistfinish.component.html'
+  templateUrl: './userregistfinish.component.html',
+  styleUrls: ['./userregistfinish.component.css'],
 })
 
 export class UserRegistFinishComponent {
